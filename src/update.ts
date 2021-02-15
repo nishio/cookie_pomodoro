9da2fc27e2d49b3e628ead36777ccdfe1e726dbf
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-02-15 11:43:46 +0900

    fix type

--- src/update.ts
+++ src/update.ts
@@ -1,5 +1,8 @@
-// @ts-ignore
-export const update = <T>(obj: { [key in T]: number }, k: T, diff: number) => {
+export const update = <T extends string>(
+  obj: { [key in T]: number },
+  k: T,
+  diff: number
+) => {
   const newObj = { ...obj };
   newObj[k] = (obj[k] ?? 0) + diff;
   return newObj;
