d73d37f27d2f8299f3059e7a6ad1733334af9e52
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-02-21 20:16:05 +0900

    add test

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,9 +1,16 @@
 import React from "react";
-import { render } from "@testing-library/react";
 import App from "../App";
 import { initializeGlobalState } from "../initializeGlobalState";
 import { load } from "../localDB";
-
+import {
+  act,
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+} from "@testing-library/react";
+import { getOnePomodoro } from "../getOnePomodoro";
+import { getGlobal } from "reactn";
 test("App", () => {
   initializeGlobalState();
   load();
@@ -12,6 +19,12 @@ test("App", () => {
 
 test("load", () => {
   initializeGlobalState();
-  load();
   render(<App />);
+  act(() => {
+    getOnePomodoro();
+  });
+  expect(getGlobal().resources.pomodoro).toBe(1);
+  // act(() => {
+  //   screen.getByText("Buy(1 pomodoro)").click();
+  // });
 });
