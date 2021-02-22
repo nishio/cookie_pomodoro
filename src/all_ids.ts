32be93d275c8fa352e44cd7a63fc07888242b36e
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-02-23 01:58:13 +0900

    tighten types

--- src/all_ids.ts
+++ src/all_ids.ts
@@ -2,7 +2,7 @@
  * switch weak / auto-generated types
  */
 
-//*
+/*
 export type TAchievementID = string;
 export type TConverterID = string;
 export type TResourceID = string;
@@ -10,7 +10,7 @@ export type TActionID = string;
 export type TRecordID = string;
 // */
 
-/*
+//*
 // auto-generated
 export type TAchievementID =
   | "pomodoro1"
@@ -28,7 +28,47 @@ export type TAchievementID =
   | "mana"
   | "three_grandma"
   | "no_mine"
-  | "day2";
+  | "day2"
+  | "day3"
+  | "day5"
+  | "day8"
+  | "day13"
+  | "day21"
+  | "day34"
+  | "day55"
+  | "day89"
+  | "day144"
+  | "day233"
+  | "pomodoro5"
+  | "pomodoro9"
+  | "pomodoro14"
+  | "pomodoro23"
+  | "pomodoro37"
+  | "pomodoro60"
+  | "pomodoro97"
+  | "pomodoro157"
+  | "pomodoro254"
+  | "pomodoro411"
+  | "pomodoro665"
+  | "pomodoro1076"
+  | "pomodoro1741"
+  | "pomodoro2817"
+  | "pomodoro4558"
+  | "cookie2"
+  | "cookie3"
+  | "cookie5"
+  | "cookie8"
+  | "cookie13"
+  | "cookie21"
+  | "cookie34"
+  | "cookie55"
+  | "cookie89"
+  | "cookie144"
+  | "cookie233"
+  | "cookie377"
+  | "cookie610"
+  | "cookie987"
+  | "cookie1597";
 export type TConverterID =
   | "grandma"
   | "coal_mine"
